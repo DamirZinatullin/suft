2a888b1d82dccc4cfe9db57c160ddf3f75d0d5c7
Zhan <bakla_zhan@mail.ru>
2021-09-10 14:13:17 +0300

    get access tokens

--- pkg/suft-api/suft-api.go
+++ pkg/suft-api/suft-api.go
@@ -3,9 +3,14 @@ package suft_api
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+)
+
+const (
+	URI string = "https://dev.gnivc.ru/tools/suft/api/v1/"
 )
 
 type ClientInterface interface {
@@ -20,13 +25,14 @@ type ClientInterface interface {
 }
 
 type suftAPI struct {
-	uri string
+	URN          string
+	Method       string
+	AccessToken  string
+	RefreshToken string
 }
 
-func NewSuftAPI(uri string) (*suftAPI, error) {
-	return &suftAPI{
-		uri: uri,
-	}, nil
+func NewSuftAPI() *suftAPI {
+	return &suftAPI{}
 }
 
 type Employee struct {
@@ -49,26 +55,38 @@ func (s *suftAPI) GetAuthToken() error {
 		Timeout: 10 * time.Second,
 	}
 
-	body := bytes.NewBuffer([]byte(`{"username":"demo@example.com","password":"demo"}`))
+	reqBody := bytes.NewBuffer([]byte(`{"username":"demo@example.com","password":"demo"}`))
 	req, err := http.NewRequest(
-		http.MethodGet,
-		s.uri,
-		body,
+		http.MethodPost,
+		fmt.Sprint(URI, "security/authenticate"),
+		reqBody,
 	)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 	req.Header.Add("Auth-method", "Password")
-	req.Header.Add("Content-Type", "application/json;charset=UTF-8")
+	req.Header.Add("Content-Type", "application/json; charset=UTF-8")
 
 	resp, err := cli.Do(req)
 	if err != nil {
 		return err
 	}
-	if (resp.StatusCode / 100) != 2 {
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
 		return errors.New("unable to get authentification tokens")
 	}
-	defer resp.Body.Close()
+
+	for _, cookie := range resp.Cookies() {
+		if cookie.Name == "Access-token" {
+			s.AccessToken = cookie.Value
+		}
+		if cookie.Name == "Refresh-token" {
+			s.RefreshToken = cookie.Value
+		}
+	}
+
 	return nil
 }
--- pkg/suft-api/suft-api_test.go
+++ pkg/suft-api/suft-api_test.go
@@ -1,1 +1,16 @@
 package suft_api
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetAuthToken(t *testing.T) {
+	suftAPI := NewSuftAPI()
+	err := suftAPI.GetAuthToken()
+	fmt.Println(suftAPI.AccessToken)
+	fmt.Println(suftAPI.RefreshToken)
+	require.NoError(t, err)
+}
