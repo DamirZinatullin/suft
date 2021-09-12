a6784793c8ee4b0429e8971f2c25711d2eb16449
Damir Zinatullin <damizinatullin@mail.ru>
2021-09-12 19:19:23 +0300

    add close config file

--- config/config.go
+++ config/config.go
@@ -21,6 +21,10 @@ func ReadConfig(path string) (*Config, error) {
 		return nil, err
 	}
 
+	if err = file.Close(); err != nil {
+		return nil, err
+	}
+
 	config := Config{}
 	if err = yaml.Unmarshal(data, &config); err != nil {
 		return nil, err
