85c25127358511ef45a2247d8e19216772e28d07
Damir Zinatullin <damizinatullin@mail.ru>
2021-09-18 17:06:09 +0300

    Добавлена инициализация клиента без записи конфига

--- cmd/suft-cli/suft-cli.go
+++ cmd/suft-cli/suft-cli.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/urfave/cli"
+	"golang.org/x/term"
 	"log"
 	"os"
+	"strings"
+	"suft_sdk/internal/auth"
+	"syscall"
 )
 
 func main() {
@@ -13,13 +19,29 @@ func main() {
 	Flags := []cli.Flag{}
 	app.Commands = []cli.Command{
 		{
-			Name:  "auth",
-			Usage: "Получение авторизационных токенов",
+			Name:  "init",
+			Usage: "Инициализация клиента",
 			Flags: Flags,
 			Action: func(c *cli.Context) error {
-				//client, err := api.NewClient("demo@example.com", "demo")
+				reader := bufio.NewReader(os.Stdin)
 
-				//
+				_, _ = os.Stdout.Write([]byte("Введите логин пользователя системы СУФТ:\n"))
+				login, _ := reader.ReadString('\n')
+				login = strings.Trim(login, "\n")
+
+				_, _ = os.Stdout.Write([]byte("Введите пароль пользователя системы СУФТ:\n"))
+				bytePassword, _ := term.ReadPassword(int(syscall.Stdin))
+				password := string(bytePassword)
+				password = strings.Trim(password, "\n")
+
+				token, err := auth.Authenticate(login, password)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(token.AccessToken, token.RefreshToken)
+				//client, err := api.NewClient("demo@example.com", "demo")
+				//fmt.Println(client,err)
+
 				return nil
 			}},
 	}
