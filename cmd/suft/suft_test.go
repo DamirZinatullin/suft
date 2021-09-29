f4904c3bd86e0e6b0441422ad949d34a07d82070
Damir Zinatullin <damizinatullin@mail.ru>
2021-09-29 15:03:21 +0300

    Добавил тесты

--- cmd/suft/suft_test.go
+++ cmd/suft/suft_test.go
@@ -91,8 +91,9 @@ type addScheduleFunc func() (*api.Schedule, error)
 type loggingTimeListFunc func() ([]*api.LoggingTime, error)
 type detailLoggingTimeFunc func() (*api.LoggingTime, error)
 type deleteLoggingTimeFunc func() error
-
-
+type submitForApproveScheduleFunc func() (*api.Schedule, error)
+type approveLoggingTimeFunc func() (*api.LoggingTime, error)
+type declineLoggingTimeFunc func() (*api.LoggingTime, error)
 
 var respSchedules schedulesFunc
 var respScheduleDetail scheduleDetailFunc
@@ -100,6 +101,9 @@ var respAddSchedule addScheduleFunc
 var respLoggingTimeList loggingTimeListFunc
 var respDetailLoggingTime detailLoggingTimeFunc
 var respDeleteLoggingTime deleteLoggingTimeFunc
+var respSubmitForApproveSchedule submitForApproveScheduleFunc
+var respApproveLoggingTime approveLoggingTimeFunc
+var respDeclineLoggingTime declineLoggingTimeFunc
 
 var exitIndicator string
 
@@ -110,7 +114,7 @@ func TestCliFunc(t *testing.T) {
 	app.ExitErrHandler = func(context *cli.Context, err error) {
 		exitIndicator = "1"
 	}
-	t.Run("Успешный вызов Schedules", func(t *testing.T){
+	t.Run("Успешный вызов Schedules", func(t *testing.T) {
 		args := []string{"", "scs"}
 		respSchedules = SuccessRespSchedules
 		err = app.Run(args)
@@ -118,7 +122,7 @@ func TestCliFunc(t *testing.T) {
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Успешный вызов Schedules с дополнительными аргументами", func(t *testing.T){
+	t.Run("Успешный вызов Schedules с дополнительными аргументами", func(t *testing.T) {
 		args := []string{"", "scs", "-p", "1", "-s", "2", "-r", "approver"}
 		respSchedules = SuccessRespSchedules
 		err = app.Run(args)
@@ -126,7 +130,7 @@ func TestCliFunc(t *testing.T) {
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Вызов Schedules с лишним аргументом", func(t *testing.T){
+	t.Run("Вызов Schedules с лишним аргументом", func(t *testing.T) {
 		args := []string{"", "scs", "fake"}
 		respSchedules = SuccessRespSchedules
 		err = app.Run(args)
@@ -134,7 +138,7 @@ func TestCliFunc(t *testing.T) {
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Ошибка при вызове метода Schedules", func(t *testing.T){
+	t.Run("Ошибка при вызове метода Schedules", func(t *testing.T) {
 		args := []string{"", "scs"}
 		respSchedules = ErrorRespSchedules
 		err = app.Run(args)
@@ -142,15 +146,15 @@ func TestCliFunc(t *testing.T) {
 		assert.Equal(t, "1", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Успешный вызов ScheduleDetail", func(t *testing.T){
-		args := []string{"", "sc", "-scid" ,"777"}
-		respScheduleDetail = SuccessRespScheduleDetail
+	t.Run("Успешный вызов ScheduleDetail", func(t *testing.T) {
+		args := []string{"", "sc", "-scid", "777"}
+		respScheduleDetail = SuccessRespDetailSchedule
 		err = app.Run(args)
 		require.NoError(t, err)
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Ошибка при вызове метода ScheduleDetail", func(t *testing.T){
+	t.Run("Ошибка при вызове метода ScheduleDetail", func(t *testing.T) {
 		args := []string{"", "sc", "-scid", "777"}
 		respScheduleDetail = ErrorRespDetailSchedule
 		err = app.Run(args)
@@ -158,23 +162,23 @@ func TestCliFunc(t *testing.T) {
 		assert.Equal(t, "1", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Передача невалидного флага в ScheduleDetail", func(t *testing.T){
-		args := []string{"", "sc", "-scid" ,"ар"}
-		respScheduleDetail = SuccessRespScheduleDetail
+	t.Run("Передача невалидного флага в ScheduleDetail", func(t *testing.T) {
+		args := []string{"", "sc", "-scid", "ар"}
+		respScheduleDetail = SuccessRespDetailSchedule
 		err = app.Run(args)
 		require.Error(t, err)
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Передача неправильного флага в ScheduleDetail", func(t *testing.T){
-		args := []string{"", "sc", "-fake" ,"777"}
-		respScheduleDetail = SuccessRespScheduleDetail
+	t.Run("Передача неправильного флага в ScheduleDetail", func(t *testing.T) {
+		args := []string{"", "sc", "-fake", "777"}
+		respScheduleDetail = SuccessRespDetailSchedule
 		err = app.Run(args)
 		require.Error(t, err)
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Успешный вызов addSchedule", func(t *testing.T){
+	t.Run("Успешный вызов addSchedule", func(t *testing.T) {
 		args := []string{"", "as", "-pid", "777"}
 		respAddSchedule = SuccessRespAddSchedule
 		err = app.Run(args)
@@ -182,7 +186,7 @@ func TestCliFunc(t *testing.T) {
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Ошибка при вызове метода addSchedule", func(t *testing.T){
+	t.Run("Ошибка при вызове метода addSchedule", func(t *testing.T) {
 		args := []string{"", "as", "-pid", "777"}
 		respAddSchedule = ErrorRespAddSchedule
 		err = app.Run(args)
@@ -190,7 +194,7 @@ func TestCliFunc(t *testing.T) {
 		assert.Equal(t, "1", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Передача невалидного флага в addSchedule", func(t *testing.T){
+	t.Run("Передача невалидного флага в addSchedule", func(t *testing.T) {
 		args := []string{"", "as", "-pid", "fake"}
 		respAddSchedule = SuccessRespAddSchedule
 		err = app.Run(args)
@@ -198,7 +202,7 @@ func TestCliFunc(t *testing.T) {
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Передача неправильного флага в addSchedule", func(t *testing.T){
+	t.Run("Передача неправильного флага в addSchedule", func(t *testing.T) {
 		args := []string{"", "as", "-fake", "777"}
 		respAddSchedule = SuccessRespAddSchedule
 		err = app.Run(args)
@@ -206,80 +210,158 @@ func TestCliFunc(t *testing.T) {
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Успешный вызов LoggingTimeList", func(t *testing.T){
-		args := []string{"", "lts", "-scid" ,"777"}
+	t.Run("Успешный вызов LoggingTimeList", func(t *testing.T) {
+		args := []string{"", "lts", "-scid", "777"}
 		respLoggingTimeList = SuccessRespLoggingTimeList
 		err = app.Run(args)
 		require.NoError(t, err)
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Успешный вызов LoggingTimeList c доп аргументами", func(t *testing.T){
-		args := []string{"", "lts", "-scid" ,"777", "-s", "5", "-p", "1"}
+	t.Run("Успешный вызов LoggingTimeList c доп аргументами", func(t *testing.T) {
+		args := []string{"", "lts", "-scid", "777", "-s", "5", "-p", "1"}
 		respLoggingTimeList = SuccessRespLoggingTimeList
 		err = app.Run(args)
 		require.NoError(t, err)
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Ошибка при вызове LoggingTimeList", func(t *testing.T){
-		args := []string{"", "lts", "-scid" ,"777"}
+	t.Run("Ошибка при вызове LoggingTimeList", func(t *testing.T) {
+		args := []string{"", "lts", "-scid", "777"}
 		respLoggingTimeList = ErrorRespLoggingTimeList
 		err = app.Run(args)
 		require.Error(t, err)
 		assert.Equal(t, "1", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Успешный вызов DetailLoggingTime", func(t *testing.T){
-		args := []string{"", "lt", "-scid" ,"777", "-ltid", "777"}
+	t.Run("Успешный вызов DetailLoggingTime", func(t *testing.T) {
+		args := []string{"", "lt", "-scid", "777", "-ltid", "777"}
 		respDetailLoggingTime = SuccessRespDetailLoggingTime
 		err = app.Run(args)
 		require.NoError(t, err)
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Вызов DetailLoggingTime без аргумента", func(t *testing.T){
-		args := []string{"", "lt", "-scid" ,"777"}
+	t.Run("Вызов DetailLoggingTime без аргумента", func(t *testing.T) {
+		args := []string{"", "lt", "-scid", "777"}
 		respDetailLoggingTime = SuccessRespDetailLoggingTime
 		err = app.Run(args)
 		require.Error(t, err)
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Ошибка при вызове DetailLoggingTime", func(t *testing.T){
-		args := []string{"", "lt", "-scid" ,"777", "-ltid", "777"}
+	t.Run("Ошибка при вызове DetailLoggingTime", func(t *testing.T) {
+		args := []string{"", "lt", "-scid", "777", "-ltid", "777"}
 		respDetailLoggingTime = ErrorRespDetailLoggingTime
 		err = app.Run(args)
 		require.Error(t, err)
 		assert.Equal(t, "1", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Успешный вызов DeleteLoggingTime", func(t *testing.T){
-		args := []string{"", "rmlt", "-scid" ,"777", "-ltid", "777"}
+	t.Run("Успешный вызов DeleteLoggingTime", func(t *testing.T) {
+		args := []string{"", "rmlt", "-scid", "777", "-ltid", "777"}
 		respDeleteLoggingTime = SuccessRespDeleteLoggingTime
 		err = app.Run(args)
 		require.NoError(t, err)
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Вызов DeleteLoggingTime без аргумента", func(t *testing.T){
-		args := []string{"", "rmlt", "-scid" ,"777"}
+	t.Run("Вызов DeleteLoggingTime без аргумента", func(t *testing.T) {
+		args := []string{"", "rmlt", "-scid", "777"}
 		respDeleteLoggingTime = SuccessRespDeleteLoggingTime
 		err = app.Run(args)
 		require.Error(t, err)
 		assert.Equal(t, "", exitIndicator)
 		exitIndicator = ""
 	})
-	t.Run("Ошибка при вызове DeleteLoggingTime", func(t *testing.T){
-		args := []string{"", "rmlt", "-scid" ,"777", "-ltid", "777"}
+	t.Run("Ошибка при вызове DeleteLoggingTime", func(t *testing.T) {
+		args := []string{"", "rmlt", "-scid", "777", "-ltid", "777"}
 		respDeleteLoggingTime = ErrorRespDeleteLoggingTime
 		err = app.Run(args)
 		require.Error(t, err)
 		assert.Equal(t, "1", exitIndicator)
 		exitIndicator = ""
 	})
-
-
+	t.Run("Успешный вызов SubmitForApproveSchedule", func(t *testing.T) {
+		args := []string{"", "s", "-scid", "777"}
+		respSubmitForApproveSchedule = SuccessRespSubmitForApproveSchedule
+		err = app.Run(args)
+		require.NoError(t, err)
+		assert.Equal(t, "", exitIndicator)
+		exitIndicator = ""
+	})
+	t.Run("Ошибка при вызове метода SubmitForApproveSchedule", func(t *testing.T) {
+		args := []string{"", "s", "-scid", "777"}
+		respSubmitForApproveSchedule = ErrorRespSubmitForApproveSchedule
+		err = app.Run(args)
+		require.Error(t, err)
+		assert.Equal(t, "1", exitIndicator)
+		exitIndicator = ""
+	})
+	t.Run("Передача невалидного флага в SubmitForApproveSchedule", func(t *testing.T) {
+		args := []string{"", "s", "-scid", "ар"}
+		respSubmitForApproveSchedule = SuccessRespDetailSchedule
+		err = app.Run(args)
+		require.Error(t, err)
+		assert.Equal(t, "", exitIndicator)
+		exitIndicator = ""
+	})
+	t.Run("Передача неправильного флага в SubmitForApproveSchedule", func(t *testing.T) {
+		args := []string{"", "s", "-fake", "777"}
+		respSubmitForApproveSchedule = SuccessRespDetailSchedule
+		err = app.Run(args)
+		require.Error(t, err)
+		assert.Equal(t, "", exitIndicator)
+		exitIndicator = ""
+	})
+	t.Run("Успешный вызов ApproveLoggingTime", func(t *testing.T) {
+		args := []string{"", "aprv", "-scid", "777", "-ltid", "777"}
+		respApproveLoggingTime = SuccessRespApproveLoggingTime
+		err = app.Run(args)
+		require.NoError(t, err)
+		assert.Equal(t, "", exitIndicator)
+		exitIndicator = ""
+	})
+	t.Run("Вызов ApproveLoggingTime без аргумента", func(t *testing.T) {
+		args := []string{"", "aprv", "-scid", "777"}
+		respApproveLoggingTime = SuccessRespApproveLoggingTime
+		err = app.Run(args)
+		require.Error(t, err)
+		assert.Equal(t, "", exitIndicator)
+		exitIndicator = ""
+	})
+	t.Run("Ошибка при вызове ApproveLoggingTime", func(t *testing.T) {
+		args := []string{"", "aprv", "-scid", "777", "-ltid", "777"}
+		respApproveLoggingTime = ErrorRespApproveLoggingTime
+		err = app.Run(args)
+		require.Error(t, err)
+		assert.Equal(t, "1", exitIndicator)
+		exitIndicator = ""
+	})
+	t.Run("Успешный вызов DeclineLoggingTime", func(t *testing.T) {
+		args := []string{"", "dcl", "-scid", "777", "-ltid", "777"}
+		respDeclineLoggingTime = SuccessRespDeclineLoggingTime
+		err = app.Run(args)
+		require.NoError(t, err)
+		assert.Equal(t, "", exitIndicator)
+		exitIndicator = ""
+	})
+	t.Run("Вызов DeclineLoggingTime без аргумента", func(t *testing.T) {
+		args := []string{"", "dcl", "-scid", "777"}
+		respDeclineLoggingTime = SuccessRespDeclineLoggingTime
+		err = app.Run(args)
+		require.Error(t, err)
+		assert.Equal(t, "", exitIndicator)
+		exitIndicator = ""
+	})
+	t.Run("Ошибка при вызове DeclineLoggingTime", func(t *testing.T) {
+		args := []string{"", "dcl", "-scid", "777", "-ltid", "777"}
+		respDeclineLoggingTime = ErrorRespDeclineLoggingTime
+		err = app.Run(args)
+		require.Error(t, err)
+		assert.Equal(t, "1", exitIndicator)
+		exitIndicator = ""
+	})
 
 }
 
@@ -321,16 +403,17 @@ func (f *fakeClient) DeleteLoggingTime(scheduleId api.ScheduleId, loggingTimeId 
 }
 
 func (f *fakeClient) SubmitForApproveSchedule(scheduleId api.ScheduleId) (*api.Schedule, error) {
-	panic("implement me")
+	return respSubmitForApproveSchedule()
 }
 
 func (f *fakeClient) ApproveLoggingTime(scheduleId api.ScheduleId, loggingTimeId api.LoggingTimeId, comment string) (*api.LoggingTime, error) {
-	panic("implement me")
+	return respApproveLoggingTime()
 }
 
 func (f *fakeClient) DeclineLoggingTime(scheduleId api.ScheduleId, loggingTimeId api.LoggingTimeId, comment string) (*api.LoggingTime, error) {
-	panic("implement me")
+	return respDeclineLoggingTime()
 }
+
 
 func SuccessRespSchedules() ([]*api.Schedule, error) {
 	return []*api.Schedule{&fakeSchedule1, &fakeSchedule2}, nil
@@ -340,7 +423,7 @@ func ErrorRespSchedules() ([]*api.Schedule, error) {
 	return nil, errors.New("error from schedules method")
 }
 
-func SuccessRespScheduleDetail() (*api.Schedule, error) {
+func SuccessRespDetailSchedule() (*api.Schedule, error) {
 	return &fakeSchedule1, nil
 }
 
@@ -352,7 +435,7 @@ func SuccessRespAddSchedule() (*api.Schedule, error) {
 	return &fakeSchedule1, nil
 }
 
-func ErrorRespAddSchedule()(*api.Schedule, error){
+func ErrorRespAddSchedule() (*api.Schedule, error) {
 	return nil, errors.New("error from addSchedule method")
 }
 
@@ -378,4 +461,28 @@ func SuccessRespDeleteLoggingTime() error {
 
 func ErrorRespDeleteLoggingTime() error {
 	return errors.New("error from DeleteLoggingTime method")
+}
+
+func SuccessRespSubmitForApproveSchedule() (*api.Schedule, error) {
+	return &fakeSchedule1, nil
+}
+
+func ErrorRespSubmitForApproveSchedule() (*api.Schedule, error) {
+	return nil, errors.New("error from SubmitForApproveSchedule method")
+}
+
+func SuccessRespApproveLoggingTime() (*api.LoggingTime, error) {
+	return &fakeLoggingTime1, nil
+}
+
+func ErrorRespApproveLoggingTime() (*api.LoggingTime, error) {
+	return nil, errors.New("error from ApproveLoggingTime method")
+}
+
+func SuccessRespDeclineLoggingTime() (*api.LoggingTime, error) {
+	return &fakeLoggingTime1, nil
+}
+
+func ErrorRespDeclineLoggingTime() (*api.LoggingTime, error) {
+	return nil, errors.New("error from DeclineLoggingTime method")
 }
