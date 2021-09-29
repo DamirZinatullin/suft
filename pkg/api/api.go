1dc602fc5ce36143e29e8e9fb8a14520af1d8714
Zhan <bakla_zhan@mail.ru>
2021-09-29 10:54:51 +0300

    вернул структуру EditLoggingTime

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -416,21 +416,7 @@ func (c *Client) SubmitForApproveSchedule(scheduleId ScheduleId) (*Schedule, err
 }
 
 func (c *Client) ApproveLoggingTime(scheduleId ScheduleId, loggingTimeId LoggingTimeId, comment string) (*LoggingTime, error) {
-	editLoggingTime := struct {
-		CommentAdminEmployee string     `json:"commentAdminEmployee"`
-		CommentEmployee      string     `json:"commentEmployee"`
-		Day1Time             float64    `json:"day1Time"`
-		Day2Time             float64    `json:"day2Time"`
-		Day3Time             float64    `json:"day3Time"`
-		Day4Time             float64    `json:"day4Time"`
-		Day5Time             float64    `json:"day5Time"`
-		Day6Time             float64    `json:"day6Time"`
-		Day7Time             float64    `json:"day7Time"`
-		ProjectId            int        `json:"projectId"`
-		StatusCode           StatusCode `json:"statusCode"`
-		Task                 string     `json:"task"`
-		WorkKindId           int        `json:"workKindId"`
-	}{
+	editLoggingTime := EditLoggingTime{
 		CommentAdminEmployee: comment,
 		StatusCode:           Approved,
 	}
@@ -474,21 +460,7 @@ func (c *Client) ApproveLoggingTime(scheduleId ScheduleId, loggingTimeId Logging
 }
 
 func (c *Client) DeclineLoggingTime(scheduleId ScheduleId, loggingTimeId LoggingTimeId, comment string) (*LoggingTime, error) {
-	editLoggingTime := struct {
-		CommentAdminEmployee string     `json:"commentAdminEmployee"`
-		CommentEmployee      string     `json:"commentEmployee"`
-		Day1Time             float64    `json:"day1Time"`
-		Day2Time             float64    `json:"day2Time"`
-		Day3Time             float64    `json:"day3Time"`
-		Day4Time             float64    `json:"day4Time"`
-		Day5Time             float64    `json:"day5Time"`
-		Day6Time             float64    `json:"day6Time"`
-		Day7Time             float64    `json:"day7Time"`
-		ProjectId            int        `json:"projectId"`
-		StatusCode           StatusCode `json:"statusCode"`
-		Task                 string     `json:"task"`
-		WorkKindId           int        `json:"workKindId"`
-	}{
+	editLoggingTime := EditLoggingTime{
 		CommentAdminEmployee: comment,
 		StatusCode:           Declined,
 	}
