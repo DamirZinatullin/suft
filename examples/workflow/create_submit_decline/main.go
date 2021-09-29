5004247d598be54198b3cd8dfb28715470f2cd6b
Zhan <bakla_zhan@mail.ru>
2021-09-29 20:05:31 +0300

    переделал workflow

--- examples/workflow/create_submit_decline/main.go
+++ examples/workflow/create_submit_decline/main.go
@@ -7,20 +7,23 @@ import (
 )
 
 func main() {
+	// инициализируем новый клиент с учётными данными Пака Юрия
 	client1, err := api.NewClient("pakua", "147753", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	periodId := api.PeriodId(381)
+	// добавляем новое расписание и выводим его на экран
+	periodId := api.PeriodId(377)
 	schedule, err := client1.AddSchedule(periodId)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println("Добавлено расписание")
+	fmt.Println("Созданное расписание")
 	fmt.Printf("%#v\n\n", *schedule)
 
-	loggingTime := api.AddLoggingTime{
+	// добавляем новый logging-time к созданному расписанию
+	loggingTime1 := api.AddLoggingTime{
 		CommentEmployee: "test15",
 		Day1Time:        1,
 		Day2Time:        1,
@@ -33,14 +36,63 @@ func main() {
 		Task:            "test15",
 		WorkKindId:      21,
 	}
-	loggingTimeCreated, err := client1.AddLoggingTime(api.ScheduleId(schedule.Id), &loggingTime)
+	loggingTimeCreated1, err := client1.AddLoggingTime(api.ScheduleId(schedule.Id), &loggingTime1)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Printf("Добавлен LoggingTime:\n")
-	fmt.Printf("%#v\n\n", *loggingTimeCreated)
 
-	//scheduleForApprove, err := client1.SubmitForApproveSchedule(api.ScheduleId(schedule.Id))
+	// добавляем ещё один logging-time к созданному расписанию
+	loggingTime2 := api.AddLoggingTime{
+		CommentEmployee: "test16",
+		Day1Time:        5,
+		Day2Time:        0,
+		Day3Time:        4,
+		Day4Time:        0,
+		Day5Time:        3,
+		Day6Time:        0,
+		Day7Time:        2,
+		ProjectId:       69753,
+		Task:            "test16",
+		WorkKindId:      21,
+	}
+	loggingTimeCreated2, err := client1.AddLoggingTime(api.ScheduleId(schedule.Id), &loggingTime2)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//получаем список объектов logging-time, относящихся к нашему расписанию и выводим их на экран
+	loggingTimes, err := client1.LoggingTimeList(api.ScheduleId(schedule.Id), nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("Список созданных Logging-Time'ов:\n")
+	for _, lt := range loggingTimes {
+		fmt.Println(*lt)
+	}
+	fmt.Println()
+	fmt.Println()
+
+	// удаляем один из объектов трудозатрат logging-time
+	err = loggingTimeCreated2.DeleteLoggingTime()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//снова получаем список объектов logging-time, относящихся к нашему расписанию и выводим их на экран
+	loggingTimes, err = client1.LoggingTimeList(api.ScheduleId(schedule.Id), nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("Список оставшихся Logging-Time'ов:\n")
+	for _, lt := range loggingTimes {
+		fmt.Println(*lt)
+	}
+	fmt.Println()
+	fmt.Println()
+
+	// отправляем расписание на утверждение руководителю/согласующему
+	// и выводим его на экран, чтобы показать, что изменился его статус
+	// здесь мы вызвали метод непосредственно у расписания
 	scheduleForApprove, err := schedule.SubmitForApproveSchedule()
 	if err != nil {
 		log.Fatalln(err)
@@ -48,20 +100,23 @@ func main() {
 	fmt.Println("Расписание, отправленное на утверждение")
 	fmt.Printf("%#v\n\n", *scheduleForApprove)
 
-	loggingTimeForApprove, err := client1.DetailLoggingTime(api.ScheduleId(scheduleForApprove.Id), api.LoggingTimeId(loggingTimeCreated.Id))
+	//показываем, что у оставшегося объекта logging-time, относящегося к нашему расписанию,
+	//также изменился статус на НУ. Выводим его на экран
+	loggingTimeForApprove, err := client1.DetailLoggingTime(api.ScheduleId(schedule.Id), api.LoggingTimeId(loggingTimeCreated1.Id))
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Printf("Трудозатраты расписания, отправленного на утверждение\n")
+	fmt.Printf("Logging-Time, находящийся на утверждении:\n")
 	fmt.Printf("%#v\n\n", *loggingTimeForApprove)
 
+	// инициализируем новый экземпляр клиента с учётными данными Никонова Олега - руководителя Пака
 	client2, err := api.NewClient("nikonovov", "147753", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	//loggingTimeApproved, err := client2.ApproveLoggingTime(api.ScheduleId(scheduleForApprove.Id), api.LoggingTimeId(loggingTimeCreated.Id), "всё хорошо")
-	loggingTimeForApproveToAdmin, err := client2.DetailLoggingTime(api.ScheduleId(scheduleForApprove.Id), api.LoggingTimeId(loggingTimeCreated.Id))
+	// получаем объект logging-time и утверждаем его. Выводим на экран
+	loggingTimeForApproveToAdmin, err := client2.DetailLoggingTime(api.ScheduleId(scheduleForApprove.Id), api.LoggingTimeId(loggingTimeCreated1.Id))
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -69,20 +124,15 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println("Отклонённый объект трудозатрат")
+	fmt.Println("Отклонённый объект трудозатрат logging-time")
 	fmt.Printf("%#v\n\n", *loggingTimeDeclined)
 
+	// После отклонения объекта трудозатрат наш изначальный объект расписания
+	// автоматически сам переводится в статус ОТКЛ. Посмотрим его.
 	scheduleDeclined, err := client2.DetailSchedule(api.ScheduleId(scheduleForApprove.Id))
 	if err != nil {
 		log.Fatalln(err)
 	}
 	fmt.Printf("Отклонённое расписание\n")
 	fmt.Printf("%#v\n\n", *scheduleDeclined)
-
-	err = loggingTimeForApprove.DeleteLoggingTime()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Printf("Трудозатрата удалена\n")
-
 }
